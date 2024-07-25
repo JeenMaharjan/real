0b8979d7f771e46f1b6dc595f0a6ec700f17609c
JeenMaharjan <106765778+JeenMaharjan@users.noreply.github.com>
2024-07-25 18:10:56 +0545

    updated

--- routes/auth.js
+++ routes/auth.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const { requireSignin } = require("../middlewares/index.js");
 const { register, login , changepassword , masterpassword , sendMessage } = require("../controllers/auth.js");
 
-router.post("/register", register);
+// router.post("/register", register);
 router.post("/login", login);
 router.post("/changepassword", changepassword);
 router.put("/masterpassword" , requireSignin ,masterpassword)
